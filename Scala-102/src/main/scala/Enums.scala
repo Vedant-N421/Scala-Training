043110190f2b70e61abc4d5166b69adc9d7d41aa
Vedant-N421 <147396311+Vedant-N421@users.noreply.github.com>
2023-10-17 17:10:10 +0100

    Progress

---
+++ Scala-102/src/main/scala/Enums.scala
@@ -0,0 +1,41 @@
+object Enums extends App{
+  object Nintendo extends Enumeration {
+    val Mario, Luigi, Peach, Bowser, Toad, DonkeyKong, Koopa, Goomba = Value
+  }
+  sealed trait Nintendo
+  case object Mario extends Nintendo
+  case object Luigi extends Nintendo
+  case object Peach extends Nintendo
+  case object Bowser extends Nintendo
+  case object Toad extends Nintendo
+  case object DonkeyKong extends Nintendo
+  case object Koopa extends Nintendo
+  case object Goomba extends Nintendo
+
+  val ls: List[Nintendo] = List(Mario, Luigi, Peach, Bowser, Toad, DonkeyKong, Koopa, Goomba)
+
+  ls.foreach(x => println(x.toString))
+
+  object TypeOfAnimalEnum extends Enumeration{
+    val Reptile, Bird, Mammal, Amphibian = Value
+  }
+
+  object canFlyEnum extends Enumeration{
+    val Yes, No, MaybeIfItFalls = Value
+  }
+
+  object canSwim extends Enumeration{
+    val Yes, No = Value
+  }
+
+  case class Animal(typ: TypeOfAnimalEnum)
+
+  sealed trait Animals
+
+  case object AnimalType extends Animals
+  case object Flies extends Animals
+  case object Swims extends Animals
+
+  val tiger = 
+
+}
